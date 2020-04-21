ec8c4fd9befd384151c82d092bd0b4551c199925
Pavel Khrustalyov <pavel_khrustalyov@mail.ru>
2020-04-21 23:15:24 +0300

    react

---
+++ docs/precache-manifest.b800db1738a449832e850aef5c8e4e18.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5112837ed717bc0875ee21061a13966b",
+    "url": "/index.html"
+  },
+  {
+    "revision": "45ee1af039bdc893f318",
+    "url": "/static/css/2.69121389.chunk.css"
+  },
+  {
+    "revision": "ddda57fba351aec30b97",
+    "url": "/static/css/main.62b1022e.chunk.css"
+  },
+  {
+    "revision": "45ee1af039bdc893f318",
+    "url": "/static/js/2.bb6c3d53.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/static/js/2.bb6c3d53.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ddda57fba351aec30b97",
+    "url": "/static/js/main.e66e3319.chunk.js"
+  },
+  {
+    "revision": "b6cf3f92a013c653c0ac",
+    "url": "/static/js/runtime-main.40fe7f5a.js"
+  },
+  {
+    "revision": "98f4a23e3ec26f2ffa25a4bfddb5b15e",
+    "url": "/static/media/logo.98f4a23e.svg"
+  }
+]);
